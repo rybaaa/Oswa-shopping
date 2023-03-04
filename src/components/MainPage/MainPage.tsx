0ce4c60278ad0e28e7d60c32fa9b0539f8dda784
rybaaa <bolshayaryba22@gmail.com>
2023-03-04 21:21:17 +0100

    feat:search field

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import s from '../MainPage/MainPage.module.scss'
+import zoom from 'assets/zoom.svg'
 
 export const MainPage = () => {
-  return <div className={s.wrapper}>hello main</div>
+  return (
+    <div className={s.wrapper}>
+      <form className={s.input}>
+        <input className={s.search} type={'text'} placeholder={'Search what you need'}></input>
+        <img className={s.zoom} src={zoom} alt={'zoom'} />
+      </form>
+    </div>
+  )
 }
