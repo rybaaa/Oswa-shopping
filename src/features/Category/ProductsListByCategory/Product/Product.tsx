64fd302c64f4ea258d7c67edc1e95e4ad64daea2
rybaaa <bolshayaryba22@gmail.com>
2023-04-11 17:18:18 +0200

    feat: success bar after adding to cart

--- src/features/Category/ProductsListByCategory/Product/Product.tsx
+++ src/features/Category/ProductsListByCategory/Product/Product.tsx
@@ -3,6 +3,7 @@ import s from './Product.module.scss'
 import Rating from '@mui/material/Rating'
 import { useAppDispatch } from '../../../../app/store'
 import { addProductToCartTC } from '../../../cart/cart-reducer'
+import { SuccessSnackbar } from '../../../../common/components/success-snackbar/SuccessSnackbar'
 
 type ProductType = {
   id: string
@@ -20,12 +21,19 @@ type ProductType = {
 
 export const Product = (props: ProductType) => {
   const dispatch = useAppDispatch()
+  const [open, setOpen] = React.useState(false)
 
   const addToCart = () => {
     dispatch(addProductToCartTC({ id: props.id, title: props.category }))
+    setOpen(true)
   }
   return (
     <div className={s.container}>
+      <SuccessSnackbar
+        open={open}
+        setOpen={setOpen}
+        text={`${props.title} was added to your cart!`}
+      />
       <img className={s.product_image} src={props.image} alt={'product'} />
       <Rating name='no-value' value={null} />
       <span className={s.product_title}>{props.title}</span>
